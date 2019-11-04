37edfea63ebe0c7537ae45007a6ad2b488611414
diepinto30 <diepinto30@gmail.com>
2019-11-04 18:57:11 -0500

    segunda carga

--- src/main/java/ec/edu/utpl/sic/arqapl/metrics/Jaccard.java
+++ src/main/java/ec/edu/utpl/sic/arqapl/metrics/Jaccard.java
@@ -8,6 +8,12 @@ public class Jaccard implements Similaritable {
         var jaccard = new JaccardDistance<String>();
         var similarity = 1.0 - jaccard.d(text1.split("\\s"),
                 text2.split("\\s"));
+        if(similarity == 1.0){
+            System.out.println("Los dos textos son similares en un: " + similarity);
+        } else if(similarity != 1.0) {
+            System.out.println("No son similares en un: " + similarity);
+        }
+
 
         return similarity;
     }
