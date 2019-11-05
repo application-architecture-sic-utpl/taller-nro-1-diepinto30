f7997fae06d71601a86d18ce1340c1332d04f615
diepinto30 <diepinto30@gmail.com>
2019-11-04 19:03:39 -0500

    Complentando taller 1, SOLID

--- src/main/java/ec/edu/utpl/sic/arqapl/TweetSimilarity.java
+++ src/main/java/ec/edu/utpl/sic/arqapl/TweetSimilarity.java
@@ -1,21 +1,46 @@
 package ec.edu.utpl.sic.arqapl;
 
-
+import ec.edu.utpl.sic.arqapl.metrics.Cosine;
 import ec.edu.utpl.sic.arqapl.metrics.Jaccard;
+import ec.edu.utpl.sic.arqapl.metrics.JardWinkleDistance;
 import ec.edu.utpl.sic.arqapl.metrics.Similaritable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TweetSimilarity {
-    private Similaritable algorithm;
+    //private Similaritable algorithm;
+    // se crea un ArrayList para ir almacenando en caso que se añade un nuevo algoritmo de similitud
+    private ArrayList<Similaritable> algorithmAll = new ArrayList<Similaritable>();
 
     public TweetSimilarity() {
-        algorithm = new Jaccard();
+        // todos los algorithm que se estan añadiendo
+        algorithmAll.add(new Jaccard());
+        algorithmAll.add(new Cosine());
+        algorithmAll.add(new JardWinkleDistance());
     }
 
-    public TweetSimilarity(Similaritable algorithm) {
-        this.algorithm = algorithm;
-    }
-
-    public double similarity(String tweet1, String tweet2) {
-        return algorithm.similarity(tweet1, tweet2);
+    public double similarity(String tweet1, String tweet2, String typeAlgorithm) {
+        // aqui usamos en el operador instanceof, sirve para conocer si un objeto es de un tipo determinado en una clase
+        // interfaz (interface)
+        double types = 0;
+        for(Similaritable algorith : algorithmAll){
+            if(typeAlgorithm.equals("jac")){
+                if(algorith instanceof Jaccard){
+                    types = algorith.similarity(tweet1, tweet2);
+                }
+            }else if(typeAlgorithm.equals("cos")){
+                if(algorith instanceof Cosine){
+                    types = algorith.similarity(tweet1, tweet2);
+                }
+            }else if(typeAlgorithm.equals("jard")){
+                if(algorith instanceof JardWinkleDistance){
+                    types = algorith.similarity(tweet1, tweet2);
+                }
+            }
+        }
+        return types;
     }
 }
+
+
