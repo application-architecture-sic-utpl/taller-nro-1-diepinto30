3ecb59370f50182765e8a2e74542ad989dc3a74f
diepinto30 <diepinto30@gmail.com>
2019-11-04 19:07:19 -0500

    Complentando taller 1, SOLID

--- src/test/java/ec/edu/utpl/sic/arqapl/tests/TweetSimilarityTest.java
+++ src/test/java/ec/edu/utpl/sic/arqapl/tests/TweetSimilarityTest.java
@@ -2,6 +2,9 @@ package ec.edu.utpl.sic.arqapl.tests;
 
 import ec.edu.utpl.sic.arqapl.TweetSimilarity;
 import static org.junit.Assert.*;
+
+import ec.edu.utpl.sic.arqapl.metrics.Similaritable;
+import ec.edu.utpl.sic.arqapl.TweetSimilarity;
 import org.junit.Test;
 
 public class TweetSimilarityTest {
@@ -11,10 +14,13 @@ public class TweetSimilarityTest {
     @Test
     public void similar() {
         var tweet1 = "Hola mundo";
-        var tweet2 = "Hola mundo";
+        var tweet2 = "Hola mundO";
         TweetSimilarity ts = new TweetSimilarity();
-            assertEquals(1.0,
-                ts.similarity(tweet1, tweet2), 0.1);
+        var typeAlgorithm = "JAC";
+        //var typeAlgorithm = "Cos";
+        //var typeAlgorithm = "Jard";
+
+        assertEquals(1.0, ts.similarity(tweet1.toLowerCase(), tweet2.toLowerCase(), typeAlgorithm.toLowerCase()), 1.0);
 
     }
 }
